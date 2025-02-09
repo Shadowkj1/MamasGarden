586e37a735234f9217af65a947083e57c6f141fb
Shadowkj1 <keshankkj@gmail.com>
2025-02-09 00:24:22 -0500

    changed time limit of logs for 10 minutes

--- hardware/Arduino/soilMoistureProject/soilMoistureProject.ino
+++ hardware/Arduino/soilMoistureProject/soilMoistureProject.ino
@@ -24,7 +24,7 @@ void setup() {
 }
 
 unsigned long previousMillis = 0; // Store the last time data was grabbed
-const long interval = 20000; // Interval to grab data (1 minute)
+const long interval = 600000; // Interval to grab data (10 minutes)
 
 void loop() {
   // Read humidity and temperature
